64b97d64a3eaa17753ab6c29611bf29bc71ddad2
Mario Leuzinger <mleuzi@outlook.com>
2025-07-10 16:13:33 +0200

    test02

--- 01-mg/03_providers.tf
+++ 01-mg/03_providers.tf
@@ -8,10 +8,10 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id = "2ebdbb9c-b4d7-4ebb-a236-b00694a909d4"
-  client_id       = "2c7df087-29e0-4041-83ce-2d858171f071"
+  subscription_id = ""
+  client_id       = ""
   client_secret   = ""
-  tenant_id       = "b109fcb2-65b1-4676-ba01-57c2ab509d59"
+  tenant_id       = ""
   features {
   }
 }
